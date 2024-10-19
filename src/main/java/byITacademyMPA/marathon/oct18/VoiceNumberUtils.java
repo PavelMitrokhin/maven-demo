e1c1225965a3d3a407f96316d5ed4963462b5577
Pavel <pamitrovel@gmail.com>
2024-10-19 21:50:03 +0300

    refactor return

--- src/main/java/byITacademyMPA/marathon/oct18/VoiceNumberUtils.java
+++ src/main/java/byITacademyMPA/marathon/oct18/VoiceNumberUtils.java
@@ -2,18 +2,17 @@ package byITacademyMPA.marathon.oct18;
 
 public class VoiceNumberUtils {
     public String voiceNumber(int number) {
-        switch (number) {
-            case 1: return  "One";
-            case 2: return  "Two";
-            case 3: return  "Three";
-            case 4: return  "Four";
-            case 5: return  "Five";
-            case 6: return  "Six";
-            case 7: return  "Seven";
-            case 8: return  "Eight";
-            case 9: return  "Nine";
-            default: return  "Invalid number";
-
-        }
+        return switch (number) {
+            case 1: yield  "One";
+            case 2: yield  "Two";
+            case 3: yield  "Three";
+            case 4: yield  "Four";
+            case 5: yield  "Five";
+            case 6: yield  "Six";
+            case 7: yield  "Seven";
+            case 8: yield  "Eight";
+            case 9: yield  "Nine";
+            default: yield  "Invalid number";
+        };
     }
 }
