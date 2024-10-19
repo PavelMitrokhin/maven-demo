f3f33be87b24cc51f366edc312d7a7601f975b05
Pavel <pamitrovel@gmail.com>
2024-10-19 22:17:01 +0300

    add HomePage and LoginPAge and 2 libs

---
+++ src/main/java/byITacademyMPA/marathon/oct19/HomePage.java
@@ -0,0 +1,18 @@
+package byITacademyMPA.marathon.oct19;
+
+public class HomePage extends BasePage{
+    protected String HOME_URL = BASE_URL + "/домашняя страница";
+    String title = "Домашняя станица";
+
+    public void open(){
+        System.out.println("Пользователь открыл страницу " + HOME_URL);
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void clickLogin(){
+        System.out.println("Пользователь кликнул по конпке Войти");
+    }
+}
---
+++ src/main/java/byITacademyMPA/marathon/oct19/LoginPage.java
@@ -0,0 +1,14 @@
+package byITacademyMPA.marathon.oct19;
+
+public class LoginPage extends BasePage {
+    protected String LOGIN_PAGE = BASE_URL + "/форма логина";
+    String title = "Форма логина";
+
+    public void open(){
+        System.out.println("Пользователь открыл страницу " + LOGIN_PAGE);
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
