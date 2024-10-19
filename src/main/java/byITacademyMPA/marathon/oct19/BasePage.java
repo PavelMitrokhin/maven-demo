fd57240d181293c4c2052d536f94f0b6d7df32b2
Pavel <pamitrovel@gmail.com>
2024-10-19 22:01:22 +0300

    refactor gitignore, add BasePage

---
+++ src/main/java/byITacademyMPA/marathon/oct19/BasePage.java
@@ -0,0 +1,8 @@
+package byITacademyMPA.marathon.oct19;
+
+public class BasePage {
+    protected String BASE_URL = "BASE URL";
+    public void open() {
+        System.out.println("User opened page " + BASE_URL);
+    }
+}
