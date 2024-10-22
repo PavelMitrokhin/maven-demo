02ee4debc9554a6cb3e56fc42b9e247a5bafcdf1
Pavel <pamitrovel@gmail.com>
2024-10-22 22:31:23 +0300

    add negative and positive methods

--- src/test/java/byITacademyMPA/svyatoslav/biz/CalculatorTest.java
+++ src/test/java/byITacademyMPA/svyatoslav/biz/CalculatorTest.java
@@ -9,7 +9,70 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CalculatorTest {
 
     @Test
-    public void test1() {
+    public void negativeTest1() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = webDriver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("Gena");
+
+        String buttonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonCalculateBy = By.xpath(buttonCalculateXpath);
+        WebElement buttonCalculateWebElement = webDriver.findElement(buttonCalculateBy);
+        buttonCalculateWebElement.click();
+    }
+
+    @Test
+    public void negativeTest2() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = webDriver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("Gena");
+
+        String inputHeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHeightBy = By.xpath(inputHeightXpath);
+        WebElement inputHeightWebElement = webDriver.findElement(inputHeightBy);
+        inputHeightWebElement.sendKeys("177");
+
+        String buttonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonCalculateBy = By.xpath(buttonCalculateXpath);
+        WebElement buttonCalculateWebElement = webDriver.findElement(buttonCalculateBy);
+        buttonCalculateWebElement.click();
+    }
+
+    @Test
+    public void negativeTest3() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = webDriver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("Gena");
+
+        String inputHeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHeightBy = By.xpath(inputHeightXpath);
+        WebElement inputHeightWebElement = webDriver.findElement(inputHeightBy);
+        inputHeightWebElement.sendKeys("177");
+
+        String inputWeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputWeightBy = By.xpath(inputWeightXpath);
+        WebElement inputWeightWebElement = webDriver.findElement(inputWeightBy);
+        inputWeightWebElement.sendKeys("75");
+
+        String buttonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonCalculateBy = By.xpath(buttonCalculateXpath);
+        WebElement buttonCalculateWebElement = webDriver.findElement(buttonCalculateBy);
+        buttonCalculateWebElement.click();
+    }
+
+    @Test
+    public void positiveTest1() {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
 
@@ -33,9 +96,9 @@ public class CalculatorTest {
         WebElement inputMaleGenderWebElement = webDriver.findElement(inputMaleGenderBy);
         inputMaleGenderWebElement.click();
 
-        String inputCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
-        By inputCalculateBy = By.xpath(inputCalculateXpath);
-        WebElement inputCalculateWebElement = webDriver.findElement(inputCalculateBy);
-        inputCalculateWebElement.click();
+        String buttonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonCalculateBy = By.xpath(buttonCalculateXpath);
+        WebElement buttonCalculateWebElement = webDriver.findElement(buttonCalculateBy);
+        buttonCalculateWebElement.click();
     }
 }
