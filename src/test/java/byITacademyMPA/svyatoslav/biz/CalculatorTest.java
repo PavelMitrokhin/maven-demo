e794dbfb379c1b9b4112faa991b3c94de36c3bcb
Pavel <pamitrovel@gmail.com>
2024-10-22 21:54:47 +0300

    refactor peremennyu, add path

--- src/test/java/byITacademyMPA/svyatoslav/biz/CalculatorTest.java
+++ src/test/java/byITacademyMPA/svyatoslav/biz/CalculatorTest.java
@@ -13,8 +13,13 @@ public class CalculatorTest {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
 
-        String xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
-        By by = By.xpath(xpath);
+        String xpathName = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By byXpathName = By.xpath(xpathName);
+        WebElement webElementName = webDriver.findElement(byXpathName);
+        webElementName.sendKeys("Gena");
+
+        String xpathButton = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By by = By.xpath(xpathButton);
         WebElement webElement = webDriver.findElement(by);
         webElement.click();
     }
